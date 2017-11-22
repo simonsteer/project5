56980d686d303ca0c09a9750a006087e70cd0287
Simon Steer <simonpsteer@gmail.com>
2017-11-21 19:15:06 -0500

    Test Commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Donut from './donut';
+
+const donuts = [
+  'Sour Cream Glazed',
+  'Honey Dip',
+  'Boston Cream'
+];
+
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          {donuts.map((donut) => {
+            return (
+              <Donut donutName={donut} />
+            )
+          })}
         </div>
       )
     }
